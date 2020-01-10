78e4d494ac7050c106142a1f610352f351af7a13
globalsys.igor <globalsys.igor@meupag.com.br>
2020-01-10 17:22:34 -0300

    loggin added to http request by creating _handleError and calling it just before call inside user_api_provider

--- lib/network/user_api_provider.dart
+++ lib/network/user_api_provider.dart
@@ -14,6 +14,7 @@ class UserApiProvider{
   }
   Future<UserResponse> getUser() async {
     try {
+      _setupInterceptor();
       Response response = await _dio.get(_endpoint);
       log('response = $response');
       return UserResponse.fromJson(response.data);
@@ -49,5 +50,36 @@ class UserApiProvider{
       errorDescription = "Unexpected error occured";
     }
     return errorDescription;
+  }
+  void _setupInterceptor() {
+    int maxCharactersPerLine = 200;
+
+    _dio.interceptor.request.onSend = (Options options) {
+      print("--> ${options.method} ${options.path}");
+      print("Content type: ${options.contentType}");
+      print("<-- END HTTP");
+      return options;
+    };
+
+    _dio.interceptor.response.onSuccess = (Response response) {
+      print(
+          "<-- ${response.statusCode} ${response.request.method} ${response.request.path}");
+      String responseAsString = response.data.toString();
+      if (responseAsString.length > maxCharactersPerLine) {
+        int iterations =
+        (responseAsString.length / maxCharactersPerLine).floor();
+        for (int i = 0; i <= iterations; i++) {
+          int endingIndex = i * maxCharactersPerLine + maxCharactersPerLine;
+          if (endingIndex > responseAsString.length) {
+            endingIndex = responseAsString.length;
+          }
+          print(responseAsString.substring(
+              i * maxCharactersPerLine, endingIndex));
+        }
+      } else {
+        print(response.data);
+      }
+      print("<-- END HTTP");
+    };
   }
 }
